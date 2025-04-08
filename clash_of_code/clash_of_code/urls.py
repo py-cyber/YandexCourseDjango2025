8779baa74ac76322e33e4fc77d3184a0d99f306e
Nitor112221 <kostya1234moi@gmail.com>
2025-04-08 12:50:20 +0500

    after merge fix
    - update localize
    - add static and media urls in debug mod in project

--- clash_of_code/clash_of_code/urls.py
+++ clash_of_code/clash_of_code/urls.py
@@ -1,4 +1,5 @@
 import django.conf
+from django.conf.urls.static import static
 from django.contrib import admin
 import django.urls
 
@@ -8,7 +9,8 @@ urlpatterns = [
     django.urls.path('users/', django.urls.include('users.urls')),
     django.urls.path('i18n/', django.urls.include('django.conf.urls.i18n')),
     django.urls.path(
-        'contests/', django.urls.include('contests.urls', namespace='contests'),
+        'contests/',
+        django.urls.include('contests.urls', namespace='contests'),
     ),
     django.urls.path('', django.urls.include('homepage.urls')),
 ]
@@ -17,3 +19,12 @@ if django.conf.settings.DEBUG:
     import debug_toolbar.toolbar
 
     urlpatterns += debug_toolbar.toolbar.debug_toolbar_urls()
+
+    urlpatterns += static(
+        django.conf.settings.STATIC_URL,
+        document_root=django.conf.settings.STATIC_ROOT,
+    )
+    urlpatterns += static(
+        django.conf.settings.MEDIA_URL,
+        document_root=django.conf.settings.MEDIA_ROOT,
+    )
